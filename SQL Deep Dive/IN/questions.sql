4a3877ee3eb3813b1677d66f82a3875d9c6ea8c2
Pallabios <102430038+Pallabios@users.noreply.github.com>
2023-02-25 03:01:09 +0100

    Update questions.sql

--- SQL Deep Dive/IN/questions.sql
+++ SQL Deep Dive/IN/questions.sql
@@ -4,7 +4,8 @@
 * Question: How many orders were made by customer 7888, 1082, 12808, 9623
 */
 
-SELECT * FROM orders;
+select sum(totalamount)
+from public.orders where customerid IN(7888, 1082, 12808, 9623);
 
 
 /*
@@ -13,4 +14,5 @@ SELECT * FROM orders;
 * Question: How many cities are in the district of Zuid-Holland, Noord-Brabant and Utrecht?
 */
 
-SELECT * FROM city;
+
+SELECT count(id) from public.city where district in ('Zuid-Holland', 'Noord-Brabant','Utrecht');
